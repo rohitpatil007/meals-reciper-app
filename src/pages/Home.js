7a6591916c4fcdd3d3210c76fed9cf722a2c7bb6
RohitPatilPerennialsys <rohit.patil@perennialsys.com>
2023-04-05 11:51:09 +0530

    dev : home-page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,18 @@
-// import classes from "./Home.module.css";
+import classes from "./Home.module.css";
+
+import { Fragment } from "react";
 
 const Home = () => {
-  return;
+  return (
+    <Fragment>
+      <div className={classes.homeWrapper}>
+        <h1>Search For Your Meal</h1>
+        <p>People Who Love to Eat Are Always the Best People</p>
+      </div>
+      <div className={classes.inputWrapper}>
+        <input type="text" placeholder="search your meal here..."></input>
+      </div>
+    </Fragment>
+  );
 };
 export default Home;
